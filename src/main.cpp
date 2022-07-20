4cdf960d36aac054436c693a372da6d1500d8973
Adam Dusty <adam.dusty27@gmail.com>
2022-07-20 09:53:58 -0700

    Working plugin loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include "lib.hpp"
-#include <iostream>
+#include <SDL2/SDL.h>
+#include <entt/entt.hpp>
+#include <fmt/format.h>
 #include <sdk/sdk.hpp>
 #undef main
 
-auto main(int argv, char **args) -> int {
-    std::string g = sdk::greeting();
-    std::cout << g << '\n';
+namespace sdk = pd::sdk;
+
+auto main() -> int {
+    fmt::print("starting\n");
+    auto reg = entt::registry{};
+
+    sdk::plugin_service plugin_service{};
+    auto path   = R"--(D:\Dev\project-duster\renderer\build\dev\librendererd.dll)--";
+    auto plugin = plugin_service.load_plugin(path);
+
+    fmt::print("loaded\n");
+
     return 0;
 }
