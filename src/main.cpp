78f13b093c15d082ff3083cc3f0b725efd80b6ee
Adam Dusty <adam.dusty27@gmail.com>
2022-07-25 20:26:26 -0700

    Got simple plugin loading with SDL window proof of concept

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include "lib.hpp"
 #include <SDL2/SDL.h>
 #include <entt/entt.hpp>
+#include <filesystem>
 #include <fmt/format.h>
+#include <fmt/std.h>
 #include <sdk/sdk.hpp>
 #undef main
 
 namespace sdk = pd::sdk;
 
 auto main() -> int {
-    fmt::print("starting\n");
-    auto reg = entt::registry{};
+    // Application initialization
+    auto reg            = entt::registry{};
+    auto plugin_service = sdk::plugin_service{};
 
-    sdk::plugin_service plugin_service{};
-    auto path   = R"--(D:\Dev\project-duster\renderer\build\dev\librendererd.dll)--";
-    auto plugin = plugin_service.load_plugin(path);
+    // Plugin loading from cwd
+    for(const auto &e: std::filesystem::directory_iterator(".\\plugins")) {
+        if(e.is_directory()) {
+            fmt::print("{}\n", e.path());
+            plugin_service.load_plugin((e.path() / std::filesystem::path{"librendererd.dll"}).string().c_str());
+        }
+    }
 
-    fmt::print("loaded\n");
+    SDL_Delay(2500);
 
     return 0;
 }
